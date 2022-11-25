a4e9ee808b84cc633eb7d3deb073bbbfcca3a575
Karthikeyareddy81 <115484346+Karthikeyareddy81@users.noreply.github.com>
2022-11-25 20:58:48 +0530

    Add files via upload

---
+++ Guess a Number/src/guess/a/number/GuessANumber.java
@@ -0,0 +1,33 @@
+package guess.a.number;
+
+import java.util.*;
+public class GuessANumber {
+    
+	public static void main(String[] args) {
+		int radom=(int)(Math.random()*100+1);
+		Scanner s=new Scanner(System.in);
+                System.out.print(radom);
+		int count=1,usrno;
+		while(count<6)
+		{
+		    System.out.println("Guess Number between 1-100:");
+		    usrno=s.nextInt();
+		    if(usrno<0||usrno>100) System.out.println("Invalid!!");
+		    else if(usrno==radom) 
+		    {
+		        System.out.println("Congrats");
+		        break;
+		    }
+		    else if (usrno>radom) System.out.println("Guess is larger Try again!!");
+		    else System.out.println("Guess is smaller Try again!!");
+		    count++;
+		}
+		switch(count){
+		    case 6: 
+		        System.out.println("Game over");
+		        break;
+		    default: 
+		        System.out.println("you got in"+(6-count)+" STARS");
+		}
+	} 
+}
